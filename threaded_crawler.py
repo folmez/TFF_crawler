adb565027baf3833e5f951336712830accb238e6
folmez <folmez@gmail.com>
2018-11-16 15:02:05 +0800

    Write to CSV file added

--- threaded_crawler.py
+++ threaded_crawler.py
@@ -1,13 +1,7 @@
 import toy_crawler
 from queue import Queue
 from threading import Thread
-
-# Single crawler worker, to be used in the threaded module
-def crawler_worker(match_queue):
-    while not match_queue.empty():
-        match_url = match_queue.get()
-        toy_crawler.single_TFF_match_url_crawler(match_url)
-        match_queue.task_done()
+import csv
 
 def threaded_crawler(start, stop, num_worker_threads):
     # Create match urls
@@ -19,26 +13,30 @@ def threaded_crawler(start, stop, num_worker_threads):
     for match in match_urls:
         match_queue.put(match)
 
+    # Create an output queue to write to the CSV file
+    output_queue = Queue()
+
     # Create crawler workers
     for i in range(num_worker_threads):
-        t = Thread(target=crawler_worker, args=(match_queue, ))
+        t = Thread(target=crawler_worker, args=(match_queue, output_queue, ))
         t.start()
-    
+
+    # Block the main thread until workers processed everything
     match_queue.join()
 
-#def worker():
-#    while True:
-#        item = q.get()
-#        do_work(item)
-#        q.task_done()
+    # Create a CSV file
+    match_output_filename = f"matches_{start}_{stop}.csv"
+    with open(match_output_filename, 'w') as f:
+        file_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow(['TFF Match ID', 'Hakem', 'Stad'])
+        while not output_queue.empty():
+            match_output = output_queue.get()
+            file_writer.writerow([match_output])
+            output_queue.task_done()
 
-#q = Queue()
-#for i in range(num_worker_threads):
-#     t = Thread(target=worker)
-#     t.daemon = True
-#     t.start()
-
-#for item in source():
-#    q.put(item)
-
-#q.join()       # block until all tasks are done
+# Single crawler worker, to be used in the threaded module
+def crawler_worker(match_queue, output_queue):
+    while not match_queue.empty():
+        match_url = match_queue.get()
+        toy_crawler. single_TFF_match_url_crawler(match_url, output_queue)
+        match_queue.task_done()
--- toy_crawler.py
+++ toy_crawler.py
@@ -1,23 +1,31 @@
 from urllib.request import urlopen
+from queue import Queue
 import match_info_extractor as mie
 
 # url = 'http://www.tff.org/Default.aspx?pageID=29&macId=' + str(i)
-def single_TFF_match_url_crawler(url):
+def single_TFF_match_url_crawler(url, output_queue):
     # There are certain patterns in the website, this code will try to capture
     # the information we need based on observed patterns
     # 'stadId=11">ANKARA 19 MAYIS<' ('stadId' occurs only once)
-    print('Getting:', url)
+    TFF_match_id_int = int(url[url.find('macId=') + len('macID=')::])
+    print('Getting #' + str(TFF_match_id_int) + ':', url)
     # Download website
     match_site_str = crawl_url(url)
 
-    # Get data
-    hakem_ids, hakem_names = mie.find_hakemler(match_site_str)
-    stad_id, stad_name = mie.find_stad(match_site_str)
-
-    # Pring extracted data from the url
-    for name in hakem_names:
-        print('Hakem:', name)
-    print('Stad:', stad_name)
+    if html_output_is_invalid(match_site_str):
+        print('This URL does not correspond to a match.')
+    else:
+        # Get data
+        hakem_ids, hakem_names = mie.find_hakemler(match_site_str)
+        stad_id, stad_name = mie.find_stad(match_site_str)
+        # Pring extracted data from the url
+        TFF_match_id_str = f"[TFF Match #{TFF_match_id_int}] "
+        for name in hakem_names:
+            print(TFF_match_id_str, 'Hakem:', name)
+        print(TFF_match_id_str, 'Stad:', stad_name)
+        match_output = str(TFF_match_id_int) + ',' + hakem_names[0] +\
+                            ',' + stad_name
+        output_queue.put(match_output)
 
 def crawl_url(url):
     response = urlopen(url)
@@ -26,3 +34,7 @@ def crawl_url(url):
     # Replace Turkish characters with question mark (?)
     html_output_str = htmlbytes.decode('utf-8', errors='replace')
     return html_output_str
+
+def html_output_is_invalid(html_output_str):
+    # 'Images/TFF/Error/tff.hatalogosu' seems to be the a unique error identifier
+    return 'Images/TFF/Error/tff.hatalogosu' in html_output_str
