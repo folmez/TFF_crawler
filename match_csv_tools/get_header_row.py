236270760305cb8aee166cc86831d79de1af4b44
folmez <folmez@gmail.com>
2018-12-11 15:31:31 +0800

    csv header row wrong order

--- match_csv_tools/get_header_row.py
+++ match_csv_tools/get_header_row.py
@@ -3,5 +3,5 @@
 def get_header_row():
     return ['TFF Match ID', 'Tarih', 'Organizasyon', 'Hakem ID', \
                     'Hakem', 'AR1 ID', 'AR1', 'AR2 ID', 'AR2', 'Dort ID', \
-                    'Dorduncu Hakem', 'Stad ID', 'Stad','Ev', 'Ev ID', \
-                    'Deplasman', 'Deplasman ID', 'Ev skor', 'Deplasman skor']
+                    'Dorduncu Hakem', 'Stad ID', 'Stad','Ev ID', 'Ev', \
+                    'Deplasman ID', 'Deplasman', 'Ev skor', 'Deplasman skor']
